8c69b6f8a15b0797f1a97259d6a47b130217912a
Karix0202 <kari.klucz@gmail.com>
2022-07-06 00:44:13 +0200

    added login

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'main_index')]
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig');
