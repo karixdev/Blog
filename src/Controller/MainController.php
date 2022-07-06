b584bd5c5bdd886b40e439db114f04bd9b7ea3d3
Karix0202 <kari.klucz@gmail.com>
2022-07-06 21:18:28 +0200

    created post card

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-        return $this->render('main/index.html.twig');
+        return $this->render('main/index.html.twig', [
+            'posts' => $postRepository->findBy([], [
+                'id' => 'DESC',
+            ])
+        ]);
     }
 }
