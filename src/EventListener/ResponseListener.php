25edf5a3b0a2bb8388f1bb7d63dce2d996279262
Karol Kluczniok <kari.klucz@gmail.com>
2022-07-17 00:57:37 +0200

    finished refreshing remember me cookie

--- src/EventListener/ResponseListener.php
+++ src/EventListener/ResponseListener.php
@@ -5,29 +5,33 @@ namespace App\EventListener;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\RememberMe\RememberMeHandlerInterface;
 
 class ResponseListener
 {
     private LoggerInterface $logger;
     private Security $security;
+    private RememberMeHandlerInterface $rememberMeHandler;
 
-    public function __construct(
-        LoggerInterface $logger,
-        Security $security
-    )
+    public function __construct(LoggerInterface $logger, Security $security, RememberMeHandlerInterface $rememberMeHandler)
     {
         $this->logger = $logger;
         $this->security = $security;
+        $this->rememberMeHandler = $rememberMeHandler;
     }
 
 
     public function onKernelResponse(ResponseEvent $responseEvent): void
     {
         $session = $responseEvent->getRequest()->getSession();
-        $this->logger->info('echo 1');
 
-        if ($session->get('remember_me_extended')) {
-            $this->logger->info('echo 2');
+        if (
+            $this->security->isGranted('ROLE_USER')
+            && $responseEvent->getRequest()->cookies->get('REMEMBERME')
+            && !$session->get('remember_me_extended')
+        ) {
+            $this->rememberMeHandler->createRememberMeCookie($this->security->getUser());
+            $session->set('remember_me_extended', true);
         }
     }
 }
